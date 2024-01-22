6674f12753d1b5e1b07546ac4a1eea4c850fc44c
Adityatv <122156191+Adityatv@users.noreply.github.com>
2024-01-22 12:49:54 +0530

    Add files via upload

---
+++ crichd/rabi.php
@@ -0,0 +1,45 @@
+
+<!--<body>-->
+<!--<script src="blast.js"></script>-->
+
+<!--<link rel="stylesheet" type="text/css " href="/assets/player/clappr/clap.css">-->
+
+
+<!--<script src="//cdn.jsdelivr.net/npm/clappr@latest/dist/clappr.min.js" type="text/javascript"></script>-->
+
+<!--<script src="//cdn.jsdelivr.net/gh/clappe/clappr-level-selector-plugin@latest/dist/level-selector.min.js"-->
+
+<!--type="text/javascript"></script>-->
+<!--<script src="//cdn.jsdelivr.net/upm/@clappr/hlsjs-playback@1.0.1/dist/hlsjs-playback.min.js"></script>-->
+<!--<div id="player" style="height: 100%; width: 100%;"></div>-->
+
+<!--<script src="//cdn.jsdelivr.net/upm/@clappr/hlsjs-playback@1.0.1/dist/hlsjs-playback.min.js"></script>-->
+
+<!--<script>-->
+
+<!--var player = new Clappr.Player({-->
+
+<!--source: 'rabi.php/m3u8=https://streams.rabbitholebd.com/live/Cricket-Live_88/Cricket-Live_88/index.m3u8', // Default source link-->
+
+<!--width: '100%',-->
+
+<!--height: '100%',-->
+
+<!--autoPlay: true,-->
+
+<!--plugins: [HlsjsPlayback, LevelSelector,-->
+
+<!--mimetype: "application/x-mpegURL",-->
+
+<!--mediacontrol: { seekbar: "#ff0000", buttons: "#eee"},-->
+
+<!--parentId: "#player",-->
+
+<!--levelSelectorConfig: true // Disable the label selector-->
+
+<!--});-->
+<!--</script>-->
+
+<!--</body>-->
+
+
